2108e76f9cfe498fdfec0844e7b0a021bd498413
daniellemendoza <67397853+daniellemendoza@users.noreply.github.com>
2022-05-31 11:36:05 -0700

    Create p1-date.js

---
+++ p1-date.js
@@ -0,0 +1,7 @@
+/*
+    CIT 281 Project 1
+    Name: Danielle Mendoza
+    
+*/
+
+console.log(["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][new Date().getUTCDay()])
